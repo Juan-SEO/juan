1113c9990b24bb0a49d8cff3b2294521f668f8bc
Juan-SEO <98541382+Juan-SEO@users.noreply.github.com>
2022-12-24 17:12:34 +0100

    Cambios de fuentes Catamaran optimizadas CSS

---
+++ assets/header.php
@@ -0,0 +1,24 @@
+<head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Catamaran&display=swap" rel="stylesheet">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="/css/estilos.css"/>
+        <title>Primera página HTML del master</title>
+        <meta name="description" content="Página de inicio de Juan Álvarez" />
+    </head>
+<body>
+    <!-- Aquí va el menú -->
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/sobre-mi.html">Sobre mi</a></li>
+                <li><a href="/contacto.html">Contacto</a></li>
+                <li><a href="/carpeta/pagina-carpeta.html">Otros datos de interes</a></li>
+                <li><a href="https://www.linkedin.com/in/juan-alvarez-gayoso/" target="_blank">Redes Sociales</a></li>
+            </ul>
+        </nav>
+    </header>
